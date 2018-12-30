afd7d49f38561f5f04b00d0854e6a0359ba8da73
YamiJorge <original.yami_jorge@hotmail.com>
2018-12-30 17:41:56 -0300

    Agregado en index.html link para crear mnuevos productos (del form previamente creado), aunque de momento no guarda las imagenes

--- producto/views.py
+++ producto/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from django.shortcuts import render, get_object_or_404
 from .models import Producto
@@ -21,9 +21,25 @@ def detalle_producto(request, pk): #Acá se recibe un request y una PK
     }
     return HttpResponse(template.render(context, request))
 
-def nuevo_producto(request):
+"""def nuevo_producto(request):
     template = loader.get_template('nuevo_producto.html')
     form = ProductForm()
+    context = {
+        'form': form
+    }
+    return HttpResponse(template.render(context, request))"""
+
+def nuevo_producto(request):
+    if request.method == 'POST': #Si la petición del método es igual a POST
+        form = ProductForm(request.POST) # entonces el form será el ProductForm solicitado
+        if form.is_valid(): #Si el form es válido
+            product = form.save() # entonces se guardará el producto ingresado
+            product.save()
+            return HttpResponseRedirect('/')# Una vez guardado redirige a la raíz
+    else:
+        form = ProductForm() # Sino, el form tomará el valor de ProductForm
+
+    template = loader.get_template('nuevo_producto.html')
     context = {
         'form': form
     }
